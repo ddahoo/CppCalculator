5da70ffea4cb412ebc8da3d7c22dace62f3a049d
ddahoo <dahoo097@gmail.com>
2017-01-02 22:25:49 -0500

    Added excessive whitespace cleanup

--- CppCalculator/CppCalculator.cpp
+++ CppCalculator/CppCalculator.cpp
@@ -13,7 +13,7 @@ using namespace std;
 
 string expression;
 
-// TODO
+// TODO:
 /* We need to improve this function so that decimal number can be recognized
    and dealt with. With this current function, the decimal will be considered as separate characters.*/
 
@@ -21,6 +21,7 @@ vector<char> SplitChars(string& expression)
 {
 	vector<char> v;
 	int i = 0;
+	// Add whitespaces
 	while (i < expression.length())
 	{
 		size_t found = expression.find_first_of("()*/+-", i);
@@ -28,7 +29,6 @@ vector<char> SplitChars(string& expression)
 		{
 			if (found == 0)
 			{
-				expression.insert(found, " ");
 				expression.insert(found + 1, " ");
 				i = found + 1;
 			}
@@ -38,6 +38,28 @@ vector<char> SplitChars(string& expression)
 				expression.insert(found + 2, " ");
 				i = found + 2;
 			}			
+		}
+		else
+		{
+			break;
+		}
+	}
+	// Cleanup
+	i = 0;
+	while (i < expression.length())
+	{
+		size_t found = expression.find_first_of(" ", i);
+		if (found < SIZE_MAX) 
+		{
+			if (expression[found - 1] == ' ')
+			{
+				expression.erase(expression.begin() + found - 1);
+			}
+			if (expression[found + 1] == ' ')
+			{
+				expression.erase(expression.begin() + found + 1);
+			}
+			i = found + 1;
 		}
 		else
 		{
